1f99f221a117b1d09007a0ddaa7e289542304f28
Andrew Sloan <sloancodes@gmail.com>
2021-01-07 16:08:20 -0700

    added styling

--- client/src/components/Jeopardy.js
+++ client/src/components/Jeopardy.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Card } from 'semantic-ui-react'
+import { Card, Reveal, Grid } from 'semantic-ui-react'
 
 
 
 
 export default () => {
   const [board, setBoard] = useState(null);
+  const [open, setOpen] = useState(false);
   
   
   useEffect(() => {
@@ -22,13 +23,38 @@ export default () => {
     setBoard(res.data.data);
   };
 
+  const styleCard = {
+    fontSize: 20,
+    color: "black",
+    textAlign: "center",
+    width: 230,
+    height: 200,
+    whiteSpace: "pre-line",
+   }
+
+const styleCardFront = {
+  fontSize: 40,
+  color: "black",
+  textAlign: "center",
+  width: 230,
+  height: 200,
+  backgroundColor: "#46D2F0",
+  justifyContent: "center", 
+  }
+
+const flex = {
+  display:"flex",
+  justifyContent: "space-around", 
+  }
 
   const renderBoard = () => {
     return board.map((category) => {
       return (
         <div>
+       
           <h1>{category.category}</h1>
           {renderCards(category.cards)}
+          
         </div>
       );
     });
@@ -37,20 +63,39 @@ export default () => {
     return cards.map((card) => {
       return (
         <div>
-          <div>{card.question}</div>
-          <p>
-            A) {card.answer1}; 
-            B) {card.answer2};
-            C) {card.answer3};
-            D) {card.correct_answer}{" "};
-          </p>
+        
+      <Reveal animated='move up'>
+      <Reveal.Content visible>
+      <Card style= {styleCardFront}>{card.points} </Card>
+      </Reveal.Content>
+      <Reveal.Content hidden>
+      <Card style= {styleCard}>
+      <br/>
+        {card.question}
+        <br/>
+        <br/>
+      A) {card.answer1}
+      <br/> 
+      B) {card.answer2}
+      <br/>
+      C) {card.answer3}
+      <br/>
+      D) {card.correct_answer}{" "}
+      <br/>
+      </Card>
+      </Reveal.Content>
+    </Reveal>
+    
+  
         </div>
      )})};
      if (!board) return <div>Loading Jeopardy</div>;
      return (
-      renderBoard()
+       <div style= {flex}>
+      {renderBoard()}
+      </div>
      )
       }
  
   
-
+     
